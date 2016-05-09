abfec5352881c3a65e5e2ce36479e69c12a78a0c
Ewan Breakey <ebreakey@gmail.com>
2016-05-09 12:49:19 +1000

    Added IO Modules
    Added IO Modules to mocha-modules and removed to-do list from Mocha-Compile

--- Mocha.js
+++ Mocha.js
@@ -1,3 +1,37 @@
+class Mocha_IO {
+	
+	/*Make multi line string into array of lines*/
+	static stringToArr(arr) {
+		return Mocha_IO.splitIntoArray(arr,"\n");
+	}
+	
+	/* Make string an array; breaking at 'token'*/
+	static splitIntoArray(str,token) {
+		var arr = [];
+		var loc = "";
+		for (var i in str) {
+			if (str[i] == token){
+				arr.push(loc);
+				loc = "";
+			} else {
+				loc += str[i];
+			}
+		}
+		arr.push(loc);
+		return arr;
+	}
+	
+	/*Returns all characters from a string*/
+	static getChars(str) {
+		var arr = [];
+		for (var i in str) {
+			arr.push(str[i]);
+		}
+		return arr;
+	}
+	
+}
+
 
 class Mocha_Compile {
 	static compileString(x) {
@@ -112,3 +146,9 @@ class Mocha_Compile {
 		return eval(x);
 	}
 }
+
+function Mocha(x){
+	return eval(Mocha_Compile.compileString(x));
+}
+
+
